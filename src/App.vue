57ed00b09b13ba9eb4cfb981dea5ff6445166f9c
mauricio <mauricio.vera.r7@gmail.com>
2022-03-05 09:50:59 -0300

    mdi

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,32 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
+    <Navbar />
   </v-app>
 </template>
 
 <script>
+import { mapActions} from 'vuex'
+import Navbar from './components/Navbar'
 
 export default {
   name: 'App',
 
+  components: {
+    Navbar
+  },
+
   data: () => ({
     //
   }),
-};
+
+  methods: {
+    ...mapActions(['get_DatosEquipos', 'get_DatosHome', 'get_DatosReq','get_DatosResp']) // inicializa action
+  },
+  mounted() {
+    this.get_DatosEquipos() // invoca action
+    this.get_DatosHome()
+    this.get_DatosReq()
+    this.get_DatosResp()
+  }
+}
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
@@ -9,6 +10,91 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+
+    async get_DatosEquipos({ commit }) {
+      try {
+        const { data: datEq } = await axios.get('/equipos.json')
+
+        // this.kpi = await datos['kpis']
+        console.log('equipos ', datEq)
+        // commit("GET_KPIS", this.kpi)
+
+        // this.orden = await datos['utimas_ordenes']
+        // console.log('utimas_ordenes ',this.orden)
+        // commit("GET_ORDEN", this.orden)
+
+        // this.devol = await datos['ultimas_devoluciones:']
+        // console.log('ultimas_devoluciones: ',this.devol)
+        // commit("GET_DEVOLU", this.devol)
+
+      } catch (error) {
+        console.log(error)
+      }
+    },
+
+    async get_DatosHome({ commit }) {
+      try {
+        const { data: datHome } = await axios.get('/home.json')
+
+        // this.kpi = await datos['kpis']
+        console.log('datHome ', datHome)
+        // commit("GET_KPIS", this.kpi)
+
+        // this.orden = await datos['utimas_ordenes']
+        // console.log('utimas_ordenes ',this.orden)
+        // commit("GET_ORDEN", this.orden)
+
+        // this.devol = await datos['ultimas_devoluciones:']
+        // console.log('ultimas_devoluciones: ',this.devol)
+        // commit("GET_DEVOLU", this.devol)
+
+      } catch (error) {
+        console.log(error)
+      }
+    },
+
+    async get_DatosReq({ commit }) {
+      try {
+        const { data: datReq } = await axios.get('/orderprocessreq.json')
+
+        // this.kpi = await datos['kpis']
+        console.log('datReq ', datReq)
+        // commit("GET_KPIS", this.kpi)
+
+        // this.orden = await datos['utimas_ordenes']
+        // console.log('utimas_ordenes ',this.orden)
+        // commit("GET_ORDEN", this.orden)
+
+        // this.devol = await datos['ultimas_devoluciones:']
+        // console.log('ultimas_devoluciones: ',this.devol)
+        // commit("GET_DEVOLU", this.devol)
+
+      } catch (error) {
+        console.log(error)
+      }
+    },
+
+    async get_DatosResp({ commit }) {
+      try {
+        const { data: datResp } = await axios.get('/orderprocessresp.json')
+
+        // this.kpi = await datos['kpis']
+        console.log('datResp ', datResp)
+        // commit("GET_KPIS", this.kpi)
+
+        // this.orden = await datos['utimas_ordenes']
+        // console.log('utimas_ordenes ',this.orden)
+        // commit("GET_ORDEN", this.orden)
+
+        // this.devol = await datos['ultimas_devoluciones:']
+        // console.log('ultimas_devoluciones: ',this.devol)
+        // commit("GET_DEVOLU", this.devol)
+
+      } catch (error) {
+        console.log(error)
+      }
+    },
+
   },
   modules: {
   }
